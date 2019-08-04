58fcdafd6d966d7450ad152bab5a9b58c7af68b3
Jared Landers <patlymlatun@gmail.com>
2019-08-04 15:35:30 -0700

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,61 @@ extern crate chrono;
 use chrono::prelude::*;
 use std::rc::Rc;
 use std::cell::Cell;
+extern crate rustc_serialize;
+use rustc_serialize::json::Json;
 
-struct Server {
+//Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+
+struct Server<'a> {
 	out: Sender,
-	count: Rc<Cell<u32>>,
+	count: Rc<Cell<usize>>,
+	hubbub: &'a Hubbub,
 }
 
-impl Handler for Server {
+impl<'a> Handler for Server<'a> {
 	
 	fn on_open(&mut self, hs: ws::Handshake) -> Result<()> {
+		println!("Client connection established.");
+		println!("{:?}", self.out.token());
 		Ok(self.count.set(self.count.get() + 1))
 	}
 	
 	fn on_message(&mut self, msg: Message) -> Result<()> {
-		println!("{:?}", msg);
+		if let Some(data) = Json::from_str(&msg.to_string()).unwrap().as_object() {
+			if let Some(convo) = data.get("convo").unwrap().as_u64() {
+				if let Some(convo) = &self.hubbub.get_convo(convo as usize) {
+					let buff = &convo.buffer;
+					if let Some(cmd) = data.get("cmd").unwrap().as_string() {
+						if let Some(txt) = data.get("txt").unwrap().as_string() {
+							if let Some(start) = data.get("start").unwrap().as_u64() {
+								let start = start as usize;
+								if let Some(end) = data.get("end").unwrap().as_u64() {
+									let end = end as usize;
+									match cmd {
+										"Replace" => {
+											if let Some(first) = buff.get(0..start) {
+												if let Some(third) = buff.get(end..buff.len()) {
+													let whole = format!("{}{}{}", first, txt, third);
+													// self.buff = whole;
+												}
+											}
+										},
+										"MoveCursor" => {
+										},
+										"RequestConvo" => {
+											
+										}
+										_ => (),
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		// self.out.send(format!("{{ \"text\": \"{}\" }}", msg))
+		self.out.close(CloseCode::Normal);
 		Ok(())
 	}
 	
@@ -38,50 +79,35 @@ impl Handler for Server {
 	
 }
 
-struct UserMessage {
-	senderid: u32,
-	text: String,
-	timestamp: DateTime<Local>,
+fn main() {
+	let count = Rc::new(Cell::new(0));
+	let mut h = Hubbub::new();
+	listen("0.0.0.0:30012", |out| Server { out: out, count: count.clone(), hubbub: &mut h, }).unwrap();
+	println!("{:?}", count);
 }
 
 struct User {
-	id: u32,
-	buffer: String,
+	id: usize,
+	convos: Vec<usize>,
+	owns: Vec<usize>,
+	administrates: Vec<usize>,
 }
 
-struct Conversation {
-	users: Vec<User>,
-	owner: u32,
-	log: Vec<UserMessage>,
-}
-
-struct Hubbub {
-	conversations: Vec<Conversation>,
-}
-
-impl Hubbub {
-	pub fn add_convo(&mut self, owner: u32) {
-		let mut convo = Conversation {
-			users: vec!(),
-			owner: owner,
-			log: vec!(),
-		};
-		convo.add_user(owner);
-		self.conversations.push(convo);
+impl User {
+	pub fn new(id: usize) -> User {
+		User {
+			id: id,
+			convos: vec!(),
+			owns: vec!(),
+			administrates: vec!(),
+		}
 	}
-}
-
-impl Conversation {
-	pub fn add_user(&mut self, id: u32) -> std::result::Result<(), String> {
-		let mut userfound = false;
-		self.users.iter().map(|u| { if u.id == id { userfound = true } });
-		if !userfound {
-			self.users.push(
-				User {
-					id: id,
-					buffer: String::new(),
-				}
-			);
+	
+	pub fn add_to_convo(&mut self, id: usize) -> std::result::Result<(), String> {
+		let mut convofound = false;
+		self.convos.iter().map(|c| { if c == &id { convofound = true } });
+		if !convofound {
+			self.convos.push(id);
 			Ok(())
 		} else {
 			Err(String::from("User already in convo"))
@@ -89,7 +115,58 @@ impl Conversation {
 	}
 }
 
-fn main() {
-	let count = Rc::new(Cell::new(0));
-	listen("0.0.0.0:30012", |out| { Server { out: out, count: count.clone() } }).unwrap()
+struct Conversation {
+	id: usize,
+	buffer: String,
+	private: bool,
+}
+
+struct Hubbub {
+	users: Vec<User>,
+	conversations: Vec<Conversation>,
+}
+
+impl Hubbub {
+	
+	pub fn new() -> Hubbub {
+		Hubbub {
+			users: vec!(),
+			conversations: vec!(),
+		}
+	}
+	
+	pub fn add_convo(&mut self, id: usize, owner: usize) {
+		if let Some(usr) = self.get_user(owner) {
+			let mut convo = Conversation {
+				id: id,
+				buffer: String::new(),
+				private: true,
+			};
+			usr.add_to_convo(id);
+			self.conversations.push(convo);
+		}
+	}
+	
+	fn get_convo(&mut self, id: usize) -> Option<&mut Conversation> {
+		for c in self.conversations.iter_mut() {
+			if c.id == id {
+				return Some(c);
+			}
+		}
+		None
+	}
+	
+	
+	pub fn get_user(&mut self, id: usize) -> Option<&mut User> {
+		for mut u in self.users.iter_mut() {
+			if u.id == id {
+				return Some(u);
+			}
+		}
+		None
+	}
+	fn send_to_all_in_convo(&mut self, convo_id: usize) -> Result<()> {
+		Ok(())
+	}
+	
 }
